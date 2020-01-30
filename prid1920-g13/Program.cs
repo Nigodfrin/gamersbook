a5a52c59a513fefbfd496d68bb62fdcc706aeec5
Nicolas <nicolas.godfrin@live.be>
2020-01-30 15:33:23 +0100

    add Neo4J Driver and connection

--- prid1920-g13/Program.cs
+++ prid1920-g13/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Neo4jClient;
+using Newtonsoft.Json.Serialization;
 using prid_1819_g13.Models;
 
 namespace prid_1819_g13
@@ -15,7 +17,26 @@ namespace prid_1819_g13
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Seed().Run();
+            var test = Neo4JConnection();
+            test.Wait();
+            Console.WriteLine(test.Result.FirstOrDefault());
+            // CreateWebHostBuilder(args).Build().Seed().Run();
+        }
+        public static async Task<IEnumerable<string>> Neo4JConnection()
+        {
+            Console.WriteLine("Connecting to Neo4J");
+            var client = new GraphClient(new Uri("http://localhost:11003/db/data/"), "neo4j", "123")
+            {
+                JsonContractResolver = new CamelCasePropertyNamesContractResolver()
+            };
+
+            Console.WriteLine(client.IsConnected);
+            await client.ConnectAsync();
+            return await client.Cypher
+                .Match("(f:Film)")
+                .Where("f.name = \"Deadpool 2\" ")
+                .Return<string>("f").ResultsAsync;
+            
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
